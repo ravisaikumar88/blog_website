9412fcfda98fd55eacc12a1f48d7d0b97d7b32b5
TELU RAVI SAI KUMAR <ravisaikumar88@gmail.com>
2023-11-01 19:13:39 +0530

    Update fc.js

--- src/fc.js
+++ src/fc.js
@@ -6,16 +6,24 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDAuH9YKwLmT43w_jVxr1F2VDM_8MgNaC4",
-  authDomain: "webp-a53a9.firebaseapp.com",
-  projectId: "webp-a53a9",
-  storageBucket: "webp-a53a9.appspot.com",
-  messagingSenderId: "102514421296",
-  appId: "1:102514421296:web:64bba422602ab8e454c7f5"
+  apiKey: "AIzaSyAlgt3KOxxv3GCCHL6m2QInnF15JG8TG4U",
+  authDomain: "webp-a6334.firebaseapp.com",
+  projectId: "webp-a6334",
+  storageBucket: "webp-a6334.appspot.com",
+  messagingSenderId: "311724638865",
+  appId: "1:311724638865:web:9f93cd15721b9a2c4db530",
+  measurementId: "G-XFYDNHZK3D"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+const analytics = getAnalytics(app);
