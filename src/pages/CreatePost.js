cae6b9d3fca04bc92745e804b33ab69bf92d4b24
TELU RAVI SAI KUMAR <ravisaikumar88@gmail.com>
2023-11-24 17:44:48 +0530

    Updated Empty Post Bug

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -3,35 +3,36 @@ import { addDoc, collection ,serverTimestamp} from "firebase/firestore";
 import { auth, db } from "../fc";
 import { useNavigate } from "react-router-dom";
 
-function CreatePost({isAuth}){
+function CreatePost({ isAuth }) {
     const [title, setTitle] = useState(""); // Initialize title with an empty string
     const [postText, setPostText] = useState("");
+    const [error, setError] = useState("");
 
     const postsCollectionRef = collection(db, "posts");
-
     let navigate = useNavigate();
 
     const createPost = async () => {
-        if(title.trim() === "" || postText.trim() === ""){
-            console.log("Title cant be empty");
+        if (title.trim() === "" || postText.trim() === "") {
+            setError("Title or Post body should not be empty.");
             return;
         }
+
+        setError(""); // Clear any previous error if submission is successful
 
         await addDoc(postsCollectionRef, {
             title,
             postText,
             timestamp: serverTimestamp(),
             author: { name: auth.currentUser.displayName, id: auth.currentUser.uid },
-            
         });
         navigate("/");
     };
 
     useEffect(() => {
         if (!isAuth) {
-          navigate("/login");
+            navigate("/login");
         }
-      }, []);
+    }, []);
 
     return (
         <div className="createPostPage">
@@ -41,26 +42,23 @@ function CreatePost({isAuth}){
                     <label> Title:</label>
                     <input
                         placeholder="Title..."
-                        value={title} // Bind the input value to the title state
-                        onChange={(event) => {
-                            setTitle(event.target.value);
-                        }}
+                        value={title}
+                        onChange={(event) => setTitle(event.target.value)}
                     />
                 </div>
                 <div className="inputGp">
                     <label> Post:</label>
                     <textarea
                         placeholder="Post..."
-                        value={postText} // Bind the textarea value to the postText state
-                        onChange={(event) => {
-                            setPostText(event.target.value);
-                        }}
+                        value={postText}
+                        onChange={(event) => setPostText(event.target.value)}
                     />
                 </div>
                 <button onClick={createPost}>Submit Post</button>
+                {error && <p className="error">{error}</p>}
             </div>
         </div>
-    )
+    );
 }
 
 export default CreatePost;
